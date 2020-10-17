4add8ac909a0c2d6098cad5da5071f525a609316
Leo Chen <64719180+chlixuan@users.noreply.github.com>
2020-10-17 22:36:14 +0800

    init

---
+++ WcfClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Service1Client client = new Service1Client();
+
+            // 使用 "client" 变量在服务上调用操作。
+
+            Console.WriteLine(""+client.HelloWcf());
+
+            // 始终关闭客户端。
+            client.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WcfService1/Service1.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+
+    public interface IService1 { }
+
+
+    /// <summary>
+    /// 假的，只是为了VS编译通过。真正实现见 Service1.svc 。
+    /// </summary>
+    public class FakeService1 : IService1
+    {
+        
+    }
+}
